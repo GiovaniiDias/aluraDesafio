83ab5cb5d199f2a822f86e1dcfe8a82d5623d2b7
GiovaniiDias <giovani.eb3@gmail.com>
2022-08-03 10:31:47 -0300

    primeiro desafio

---
+++ Comparar/comparando.js
@@ -0,0 +1,50 @@
+/*
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (COMPARAR O numeroUm e a stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (COMPARAR O numeroTrinta e a stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (COMPARAR O numeroDez e a stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
+*/
+
+let nUm = 1
+let sUm = '1'
+
+if (1 == "1") {
+    console.log ("As variaveis nUm e sUm, tem o mesmo valor, mas tipos diferentes.")
+} else {
+    console.log("As variaveis nUm e sUm não tem o mesmo valor!")
+}
+
+let nTrinta = 30
+let sTrinta = '30'
+if ( 30 === "30" ) {
+    console.log('As variáveis nTrinta e sTrinta tem o mesmo valor e mesmo tipo!')
+  } else {
+    console.log('As variáveis',  nTrinta , 'e', sTrinta  ,'não tem o mesmo tipo!')
+}
+let nDez = 10
+let sDez = '10'
+if (nDez==sDez) {
+    console.log('As variáveis' , nDez , 'e' , sDez ,'tem o mesmo valor, mas tipos diferentes!')
+  } else {
+    console.log('As variáveis' , 10 , 'e', "10" ,  'não tem o mesmo valor!')
+  }
