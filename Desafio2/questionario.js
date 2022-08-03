f7c9306442012849219f6e9d3163e7d99061bdf1
GiovaniiDias <giovani.eb3@gmail.com>
2022-08-03 16:55:29 -0300

    exercicio dois do desafio Alra

---
+++ Desafio2/questionario.js
@@ -0,0 +1,16 @@
+/*
+- Qual o seu nome ?
+- Quantos anos você tem?
+- Qual linguagem de programação você está estudando?
+
+no final
+"Olá [nome], você tem [idade] anos e já está aprendendo [linguagem]!"
+
+
+*/
+
+
+let formula = window.document.getElementById("quiz")
+let nome = window.document.getElementById("nome")
+let idade = window.document.getElementById("idade")
+let lipr = window.document.getElementById("lang")
