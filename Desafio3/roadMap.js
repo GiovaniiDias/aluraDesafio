433d64fc1f04a611d992748d1d27e9f6ba5adf5a
GiovaniiDias <giovani.eb3@gmail.com>
2022-08-05 16:47:36 -0300

    concluindo 3/7

--- Desafio3/roadMap.js
+++ Desafio3/roadMap.js
@@ -9,6 +9,7 @@ var button4 = document.querySelector(".f4");
 var button04 = document.querySelector(".b4");
 var parag = document.querySelector("p");
 var letra = document.querySelector("h4")
+var final = document.querySelector("h6")
 
 
 button.onclick = function () {
@@ -53,3 +54,34 @@ button03.onclick = function () {
     button04.style.display = "block";
     letra.textContent = ("Vai se especializar ou ser FullStack")
 }
+
+const area = prompt("Você quer seguir para área de 'Front-End' ou de 'Back-End'? Digite o nome da área:");
+let linguagem = "";
+if (area === "Front-End"){
+    linguagem = prompt("Você quer aprender React ou Vue?");
+}
+else if (area === "Back-End"){
+    linguagem = prompt("Você quer aprender C# ou Java?");
+}
+else {
+    alert("Você não inseriu uma área válida!");
+}
+
+const especialidadeOuFullstack = prompt("Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack");
+if (especialidadeOuFullstack == 1){
+    alert(`Continue se especializando em ${linguagem} para dominar a área de ${area}!`);
+}
+else if (especialidadeOuFullstack == 2){
+    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`);
+}
+else {
+    alert("Você não inseriu um valor válido!");
+}
+
+let msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
+while (msg === "ok"){
+    let novaTecnologia = prompt("Qual?");
+    alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`)
+    msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
+}
+LÇGKHNIKKNKGI, K, K KKFJGJGJR~JMLKOHK, FK, KHOKHKHKHJTH.ÇLKHLLHK
